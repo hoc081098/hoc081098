a5d320a813cf8ba3d4c9791a8f80104e7a155e17
hoc081098 <hoc081098@gmail.com>
2023-01-04 10:27:28 +0700

    update countdown

--- countdown/lib/messages.dart
+++ countdown/lib/messages.dart
@@ -21,4 +21,69 @@ final messages = <String>[
                      행운 만땅 행복 가득
 Happy New Year 2023, ngập tràn may mắn, hạnh phúc đong đầy.
   ''',
+  ''',
+           ③
+           ②
+           ①
+        。☆2023☆ 。
+        ★。＼｜／。★
+        。 HAPPY 。
+        ★。／｜＼。★
+        。☆ ☆。
+        。☆♡♡☆ 。
+        ★。＼｜／。★
+        。 Quý Mão 。
+        ★。／｜＼。★
+        。☆ ☆。
+Nâng cốc chúc mừng chào năm mới!
+Tiền vào như nước lộc vô biên!
+  ''',
+  '''
+  ♡━┓┏━┓
+  ┃C┃┃M┃
+  ┃H┃┃Ừ┃
+  ┃Ú┃┃N┃
+  ┃C┃┃G┃
+  ┗━┛┗━♡
+  ^o^
+  ☆★+○─○+☆★
+  NĂM MỚI!!
+☆★+─2023─+☆★
+  ''',
+  '''
+____♥♥♥_____♥♥♥_An_____
+__♥_____♥_♥_____♥_Khang
+__♥______♥______♥_Thinh
+___♥___Happy___♥Vuong__
+_____♥_2023__♥Tan______
+_______♥___♥_Tai_______
+_________♥_Tan Loc_________
+  ''',
+  '''
+            ♡━┓┏━┓
+            ┃C┃┃M┃
+            ┃H┃┃Ừ┃
+            ┃Ú┃┃N┃
+            ┃C┃┃G┃
+            ┗━┛┗━♡
+            ☆★+○─○+☆★
+            NĂM MỚI!!
+            ☆★+───+☆★
+※Cung Chúc Tân Xuân Phước Vĩnh Cửu
+※Chúc Trong Gia Quyến Được An Khương
+※Tân Niên Lai Đáo Đa Phú Quý
+※Xuân Đến An Khương Vạn Thọ Tường
+__2022 Phat Tai – Phat loc__
+  ''',
+  '''
+    (¯`•._.•[Happy]•._.•´¯)
+    ¨°o.O New O.o°
+    ••.•´¯`•.••Year 2022 ••.•´¯`•.••
+      Cung chúc tân niên,
+      Vạn sự bình yên,
+      Hạnh phúc vô biên,
+      Vui vẻ triền miên,
+      Kiếm được nhiều tiền,
+      Sung sướng như tiên.
+  '''
 ];
