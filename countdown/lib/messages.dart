b62d9cec2b45da37d2accfe1d0fbc1d0f922046d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2024-01-28 14:52:29 +0700

    Update main.dart [skip ci]

--- countdown/lib/messages.dart
+++ countdown/lib/messages.dart
@@ -3,7 +3,7 @@ final messages = <String>[
   '''
                   (..*˚ ˛•Happy•˚ ˛*... )
                   (......☆New Year☆... )
-                  »——(¯`2023´¯)——»
+                  »——(¯`2024´¯)——»
                   Đong cho đầy Hạnh Phúc
                   Gói cho trọn Lộc Tài
                   Giữ cho mãi An Khang
@@ -17,15 +17,15 @@ final messages = <String>[
                       ┏*ⓗⓐⓟⓟⓨ*┓
                        ┃★*ⓝⓔⓦ*★┃
                        ┗━ⓨⓔⓐⓡ━┛
-                     (*^0^)2023(^0^*)
+                     (*^0^)2024(^0^*)
                      행운 만땅 행복 가득
-Happy New Year 2023, ngập tràn may mắn, hạnh phúc đong đầy.
+Happy New Year 2024, ngập tràn may mắn, hạnh phúc đong đầy.
   ''',
   ''',
            ③
            ②
            ①
-        。☆2023☆ 。
+        。☆2024☆ 。
         ★。＼｜／。★
         。 HAPPY 。
         ★。／｜＼。★
@@ -48,14 +48,14 @@ Tiền vào như nước lộc vô biên!
   ^o^
   ☆★+○─○+☆★
   NĂM MỚI!!
-☆★+─2023─+☆★
+☆★+─2024─+☆★
   ''',
   '''
 ____♥♥♥_____♥♥♥_An_____
 __♥_____♥_♥_____♥_Khang
 __♥______♥______♥_Thinh
 ___♥___Happy___♥Vuong__
-_____♥_2023__♥Tan______
+_____♥_2024__♥Tan______
 _______♥___♥_Tai_______
 _________♥_Tan Loc_________
   ''',
@@ -73,12 +73,12 @@ _________♥_Tan Loc_________
 ※Chúc Trong Gia Quyến Được An Khương
 ※Tân Niên Lai Đáo Đa Phú Quý
 ※Xuân Đến An Khương Vạn Thọ Tường
-__2022 Phat Tai – Phat loc__
+__2024 Phat Tai – Phat loc__
   ''',
   '''
     (¯`•._.•[Happy]•._.•´¯)
     ¨°o.O New O.o°
-    ••.•´¯`•.••Year 2022 ••.•´¯`•.••
+    ••.•´¯`•.••Year 2024 ••.•´¯`•.••
       Cung chúc tân niên,
       Vạn sự bình yên,
       Hạnh phúc vô biên,
